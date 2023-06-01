fa8e5aa312f2d9eaf3f9bbed0c31ba36b7e35f0f
Kateryna <the.woman.zp@gmail.com>
2023-06-01 15:23:17 +0300

    fix bugs for AdminLTE

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -30,8 +30,8 @@ scratch. This page gets rid of all links and provides the needed markup only.
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href={{ asset('index3.html') }} class="brand-link">
-          <img src={{ asset('adminlte/dist/img/LogoAdmin.png') }} alt="AdminLTE Logo"
-            class="brand-image img-circle elevation-3" style="opacity: .8">
+          <img src={{ asset('admin_assets/img/LOGO.png') }} alt="Logo" class="brand-image img-circle elevation-3"
+            style="opacity: .8">
           <span class="brand-text font-weight-light">Happy B-day</span>
         </a>
 
@@ -96,17 +96,19 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <script src={{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}></script>
 
 
+    <script src="{{ asset('adminlte/plugins/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
     <!-- daterangepicker -->
     <script src="{{ asset('adminlte/plugins/moment/moment.min.js')}}"></script>
     <script src="{{ asset('adminlte/plugins/daterangepicker/daterangepicker.js')}}"></script>
 
     <script>
-      // $(function () {
-      //   //Date picker
-      //   $('#birthday').datetimepicker({
-      //     format: 'L'
-      //   });
-      // })
+      $(function () {
+        //   //Date picker
+        //   $('#birthday').datetimepicker({
+        //     format: 'L'
+        //   });
+        bsCustomFileInput.init();
+      })
     </script>
 
     <!-- AdminLTE App -->
