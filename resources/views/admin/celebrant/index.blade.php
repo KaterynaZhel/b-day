2e195bd8b0c30945a5607213860b7936e7c99e5d
dari777 <darya.filonenko86@gmail.com>
2023-06-05 22:04:55 +0300

    resolve conflicts index.blade.php

--- resources/views/admin/celebrant/index.blade.php
+++ resources/views/admin/celebrant/index.blade.php
@@ -11,35 +11,17 @@
         <div class="card-header">
             <h3 class="card-title">Таблиця іменинників</h3>
         </div>
-<<<<<<< HEAD
-
         <div class="card-body">
             <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
                 <div>
-                    <!-- <div id="example1_filter" class="dataTables_filter"> -->
-=======
-        <div class="card-body">
-            <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
-                <div>
->>>>>>> develop
                     <label>
                         Пошук:
                         <input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1">
                     </label>
-<<<<<<< HEAD
-
-                    <a class="btn btn-primary btn-sm float-right" href="{{ route('admin.celebrant.create') }}"
-                        align="right">
-                        Створити іменинника
-                    </a>
-
-                    <!-- </div> -->
-=======
                     <a class="btn btn-primary btn-sm float-right" href="{{ route('admin.celebrant.create') }}"
                         align="right">
                         Додати іменинника
                     </a>
->>>>>>> develop
                 </div>
                 <div class=" row">
                     <div class="col-sm-12">
@@ -81,11 +63,7 @@
                                                 <img src="{{ asset('adminlte/dist/img/avatar.png') }}" style="height:30px;"
                                                     alt="">
                                             @else
-<<<<<<< HEAD
-                                                <img src="{{ public_path($celebrant->photo) }}" alt="">
-=======
                                                 <img src="{{ asset(Storage::url($celebrant->photo)) }}" alt="">
->>>>>>> develop
                                             @endif
                                         </td>
                                         <td>{{ $celebrant->lastname }}</td>
@@ -115,10 +93,6 @@
                                         </td>
                                     </tr>
                                 @endforeach
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
                             </tbody>
                             <tfoot>
                                 <tr>
@@ -130,21 +104,11 @@
                                     <th rowspan="1" colspan="1">Дата народження</th>
                                     <th rowspan="1" colspan="1">Роль</th>
                                     <th rowspan="1" colspan="1"></th>
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
                                 </tr>
                             </tfoot>
                         </table>
                     </div>
                 </div>
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> develop
                 <div class="row">
                     <div class="col-sm-12 col-md-5">
                         <div class="dataTables_info" id="example1_info" role="status" aria-live="polite">
@@ -178,10 +142,6 @@
                     </div>
                 </div>
             </div>
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
         </div>
         <!-- TODO: create pagination -->
         <!-- {{ $celebrants->links() }} -->
