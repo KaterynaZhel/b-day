5a7e0378b2dca8d1ad784bad975feac9318ecc1c
dari777 <darya.filonenko86@gmail.com>
2023-06-08 22:55:57 +0300

    customize admins login page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,16 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function authenticated()
+    {
+        if (Auth::user()->role == '1') {
+            return redirect()->route('admin.celebrant.index')->with('message', 'Welcome to AdminsDashbord');
+        } else {
+            return redirect()->route('home')->with('message', 'You are logged in successfully!');
+        }
+    }
 
     /**
      * Create a new controller instance.
