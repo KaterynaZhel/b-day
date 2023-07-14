23c08306e6a305c18dc82c25badf5eed7dfa418f
Kateryna <the.woman.zp@gmail.com>
2023-07-15 01:38:11 +0300

    add filling date for publishing greeting company

--- app/Models/GreetingCompany.php
+++ app/Models/GreetingCompany.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Enums\GreetingStatusEnum;
@@ -15,4 +16,30 @@ class GreetingCompany extends Model
     protected $casts = [
         'status' => GreetingStatusEnum::class
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(
+            function ($model) {
+                $model->publish_at = self::GreetingDate($model->celebrant_id);
+            }
+        );
+    }
+    /**
+     * Generate date for publishing greeting company
+     * @param int $celebrant_id
+     * @return mixed
+     */
+    public static function GreetingDate(int $celebrant_id)
+    {
+        $birthday            = Celebrant::where('id', $celebrant_id)->value('birthday');
+        $birthdayCurrentYear = Carbon::create($birthday)->year(now()->format('Y'))->format('Y-m-d');
+        if (Carbon::create($birthdayCurrentYear)->gt(Carbon::now())) {
+            return $birthdayCurrentYear;
+        } else {
+
+            return Carbon::create($birthdayCurrentYear)->addYear(1)->format('Y-m-d');
+        }
+    }
 }
--- resources/views/includes/admin/showGreetingsCompany.blade.php
+++ resources/views/includes/admin/showGreetingsCompany.blade.php
@@ -1,9 +1,9 @@
 <div class="card">
     @if (session('success'))
-        <div class="alert alert-success" role="alert">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h4><i class="icon fa fa-check"></i>{{ session('success') }}</h4>
-        </div>
+    <div class="alert alert-success" role="alert">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-check"></i>{{ session('success') }}</h4>
+    </div>
     @endif
     <div class="card-header">
         <h3 class="card-title">Таблиця привітань від Компанії</h3>
@@ -18,33 +18,33 @@
                             <tr>
                                 <th class="sorting sorting_asc" tabindex="0" aria-controls="example1" rowspan="1"
                                     colspan="1" aria-sort="ascending">id</th>
-                                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1"
-                                    colspan="1">Текст вітання</th>
-                                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1"
-                                    colspan="1">Назва Компанії</th>
-                                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1"
-                                    colspan="1">Статус вітання</th>
-                                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1"
-                                    colspan="1">Інструменти</th>
+                                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1">Текст
+                                    вітання</th>
+                                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1">Назва
+                                    Компанії</th>
+                                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1">Дата
+                                    публікації</th>
+                                <th class="sorting" tabindex="0" aria-controls="example1" rowspan="1" colspan="1">
+                                    Інструменти</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($greetingsCompany as $greeting)
-                                <tr class="odd">
-                                    <td class="dtr-control sorting_1" tabindex="0">{{ $greeting->id }}</td>
-                                    <td>{{ $greeting->message_company }}</td>
-                                    <td>{{ $greeting->name_company }}</td>
-                                    <td>{{ $greeting->status }}</td>
-                                    <td class="project-actions text-right">
-                                        <form method="post" action="#">
-                                            @method('delete')
-                                            @csrf
-                                            <button type="submit" class="btn destroy-btn btn-danger btn-sm">
-                                                <i class="fas fa-trash"></i>
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
+                            <tr class="odd">
+                                <td class="dtr-control sorting_1" tabindex="0">{{ $greeting->id }}</td>
+                                <td>{{ $greeting->message_company }}</td>
+                                <td>{{ $greeting->name_company }}</td>
+                                <td>{{ $greeting->publish_at }}</td>
+                                <td class="project-actions text-right">
+                                    <form method="post" action="#">
+                                        @method('delete')
+                                        @csrf
+                                        <button type="submit" class="btn destroy-btn btn-danger btn-sm">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
                             @endforeach
                         </tbody>
                     </table>
