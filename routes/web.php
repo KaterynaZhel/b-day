89d9ca7ffe915c45ce653287978547a89874fb88
Kateryna <the.woman.zp@gmail.com>
2023-07-21 23:33:48 +0300

    added displaying on admin panel greetings companies

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::prefix('admin')->middleware(['auth', 'isAdmin'])->group(function () {
         'celebrants' => App\Http\Controllers\Admin\Celebrant\CelebrantController::class,
         'greetings' => App\Http\Controllers\Admin\Greeting\GreetingController::class,
         'celebrants.greetingsCompany' => App\Http\Controllers\Admin\Greeting\GreetingCompanyController::class,
+        'mainGreetingsCompany' => App\Http\Controllers\Admin\Greeting\MainGreetingCompanyController::class,
     ], [
         'as' => 'admin'
     ]);
