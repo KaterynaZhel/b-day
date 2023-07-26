cc72d24c89d3efdaf1d9f1b629639ac63f3dcce7
Kateryna <the.woman.zp@gmail.com>
2023-07-26 23:42:05 +0300

    added a company name filter in the Company Greetings table

--- app/Http/Controllers/Admin/Greeting/MainGreetingCompanyController.php
+++ app/Http/Controllers/Admin/Greeting/MainGreetingCompanyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Greeting;
 
 use App\Models\GreetingCompany;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 
 class MainGreetingCompanyController extends Controller
@@ -11,9 +12,19 @@ class MainGreetingCompanyController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $greeting = GreetingCompany::orderBy('id', 'desc')->paginate(20);
+        if ($request->ajax()) {
+            return GreetingCompany::select("name_company as value", "id")
+                ->where('name_company', 'LIKE', '%' . $request->get('search') . '%')
+                ->get();
+        }
+        $greeting = GreetingCompany::orderBy('id', 'desc');
+        if ($request->filled('search')) {
+            $greeting->where('name_company', $request->get('search'));
+        }
+        $greeting = $greeting->paginate(20);
+
         return view('admin.greetingsCompany.index', ['greetingsCompany' => $greeting]);
     }
 
