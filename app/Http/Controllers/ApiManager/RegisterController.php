b43e60f5b7cba7d60d133430ddc4b8f432ff8a36
Kateryna <the.woman.zp@gmail.com>
2023-08-29 16:42:10 +0300

    create API for manager registration

---
+++ app/Http/Controllers/ApiManager/RegisterController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers\ApiManager;
+
+use App\Http\Controllers\Controller;
+use App\Models\Company;
+use App\Providers\RouteServiceProvider;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Register Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles the registration of new users as well as their
+    | validation and creation. By default this controller uses a trait to
+    | provide this functionality without requiring any additional code.
+    |
+    */
+
+    use RegistersUsers;
+
+    /**
+     * Where to redirect users after registration.
+     *
+     * @var string
+     */
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8'],
+            'company_name' => ['required', 'string', 'max:255', 'unique:companies,name'],
+            'company_site' => ['url', 'nullable', 'max:255'],
+
+        ]);
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    protected function create(array $data)
+    {
+        $company = Company::create([
+            'name' => $data['company_name'],
+            'site' => $data['company_site'] ?? null,
+        ]);
+
+        return User::create([
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+            'role' => User::MANAGER_ROLE,
+            'company_id' => $company->id,
+        ]);
+    }
+}
