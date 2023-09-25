beff6056996e44ff178653703897405528fceb0b
dari777 <darya.filonenko86@gmail.com>
2023-09-25 16:29:55 +0300

    connect Greeting companies DB and Company DB

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -5,14 +5,20 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Celebrant;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
     use HasFactory;
     protected $fillable = ['name', 'site'];
 
-    public function celebrants()
+    public function celebrants(): HasMany
     {
         return $this->hasMany(Celebrant::class, 'company_id', 'id');
+    }
+
+    public function greetingsCompany(): HasMany
+    {
+        return $this->hasMany(GreetingCompany::class, 'company_id', 'id');
     }
 }
--- app/Models/GreetingCompany.php
+++ app/Models/GreetingCompany.php
@@ -4,17 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 
 class GreetingCompany extends Model
 {
     use HasFactory;
-    protected $fillable = ['message_company', 'name_company', 'celebrant_id'];
+    protected $fillable = ['message_company', 'name_company', 'celebrant_id', 'company_id'];
 
 
-    public function celebrant()
+    public function celebrant(): BelongsTo
     {
         return $this->belongsTo(Celebrant::class);
     }
 
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
