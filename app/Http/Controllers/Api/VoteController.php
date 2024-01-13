1404e9a5371aef897fdc03d95bc6bc1b23737d98
dari777 <darya.filonenko86@gmail.com>
2024-01-13 16:51:29 +0200

    record voting results with limit of votes

--- app/Http/Controllers/Api/VoteController.php
+++ app/Http/Controllers/Api/VoteController.php
@@ -6,8 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ManagerResources\GiftResource;
 use App\Http\Resources\VoteResource;
 use App\Models\Vote;
-
-
+use Illuminate\Http\Request;
+use App\Models\Celebrant;
+use App\Models\VotingResult;
 
 class VoteController extends Controller
 {
@@ -30,5 +31,47 @@ class VoteController extends Controller
         $votingHash = substr($hash, 64);
         $vote       = Vote::where('hash', $votingHash)->first();
         return new VoteResource($vote);
+    }
+
+    public function voting(Request $request, string $hash)
+    {
+        // Extracting the last 32 characters as the voting hash
+        $votingHash   = substr($hash, -32);
+        $vote         = Vote::where('hash', $votingHash)->first();
+
+        // Extracting the first 64 characters as the employee hash
+        $employeeHash = substr($hash, 0, 64);
+        $employee     = Celebrant::where('hash', $employeeHash)->first();
+
+        // Return 404 if the celebrant is not found
+        if (!$employee) {
+            abort(404);
+        }
+
+        // Check if the celebrant has already voted
+        if ($this->hasAlreadyVoted($employee)) {
+            return response()->json(['error' => 'You have already voted.'], 422);
+        }
+
+        // Validate the gift_id
+        $request->validate([
+            'gift_id' => 'required|numeric|exists:gifts,id',
+        ]);
+
+        $votingResult = new VotingResult();
+        $votingResult->celebrant_id = $employee->id;
+        $votingResult->gift_id = $request->input('gift_id');
+
+        if ($vote) {
+            $votingResult->vote_id = $vote->id;
+        }
+        $votingResult->save();
+
+        return response()->json(['message' => 'Vote recorded successfully.']);
+    }
+
+    private function hasAlreadyVoted($celebrant)
+    {
+        return VotingResult::where('celebrant_id', $celebrant->id)->exists();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -75,4 +75,4 @@ Route::middleware(['auth:api', 'isManager'])->prefix('manager')->group(function 
 
 Route::get('/vote/{hash}', [App\Http\Controllers\Api\VoteController::class, 'show']);
 Route::get('/vote/statistics/{hash}', [App\Http\Controllers\Api\VoteController::class, 'statistics']);
-
+Route::post('/vote/voting/{hash}', [App\Http\Controllers\Api\VoteController::class, 'voting']);
