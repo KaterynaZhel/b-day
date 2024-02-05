1aad03250d5f25b1ee0ea278ec6927a2c2fb7c36
Kateryna <the.woman.zp@gmail.com>
2024-02-05 15:55:29 +0200

    added filters by name and date to the list of greetings

--- app/Http/Controllers/ApiManager/GreetingController.php
+++ app/Http/Controllers/ApiManager/GreetingController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\ApiManager;
 
 use App\Http\Controllers\Controller;
+use App\Http\Filters\GreetingFilter;
+use App\Http\Requests\FilterGreetingRequest;
 use App\Http\Resources\GreetingResource;
+use App\Models\Celebrant;
 use App\Models\Greeting;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
@@ -13,13 +16,14 @@ class GreetingController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(FilterGreetingRequest $request, GreetingFilter $filter)
     {
 
         $greetings = Greeting::orderBy('greetings.id', 'desc')
             ->select('greetings.*')
             ->where('company_id', '=', Auth::user()->company_id)
             ->leftjoin('celebrants', 'greetings.celebrant_id', '=', 'celebrants.id')
+            ->filter($filter)
             ->paginate(10);
         return GreetingResource::collection($greetings);
 
--- app/Models/Greeting.php
+++ app/Models/Greeting.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use App\Concerns\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Greeting extends Model
 {
     use HasFactory;
+    use Filterable;
     protected $fillable = ['name', 'message', 'celebrant_id'];
 
 }
